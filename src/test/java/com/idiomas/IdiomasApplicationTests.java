240b72bd9a4235869d93b1b426880c038670d3da
Luis <luismarcelo.v.c@hotmail.com>
2023-11-03 16:07:54 -0300

    Aplicacion de Idiomas

---
+++ src/test/java/com/idiomas/IdiomasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idiomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdiomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
